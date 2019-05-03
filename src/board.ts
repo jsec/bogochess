3acad83af61356cafb9abece6e8ceee090688cac
Jarrod Seccombe <jarrod@cynical.software>
2019-05-03 12:15:10 -0400

    remove setInitialState spike

--- src/board.ts
+++ src/board.ts
@@ -3,17 +3,4 @@ import IPiece from './interfaces/piece.interface';
 
 export default class Board {
   constructor() {}
-
-  setInitialState(color: Color): void {
-    let pawnRow: number;
-    let royalRow: number;
-
-    if (color === Color.Light) {
-      pawnRow = 6;
-      royalRow = 7;
-    } else {
-      pawnRow = 1;
-      royalRow = 0;
-    }
-  }
 }
